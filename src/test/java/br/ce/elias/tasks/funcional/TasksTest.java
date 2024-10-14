dea76dc02203791bab26de9d58542a25eb23f3e6
Elias Soares <eliassoaressouza@gmail.com>
2024-10-14 11:31:54 -0300

    incluindo selenium grid

--- src/test/java/br/ce/elias/tasks/funcional/TasksTest.java
+++ src/test/java/br/ce/elias/tasks/funcional/TasksTest.java
@@ -1,5 +1,7 @@
 package br.ce.elias.tasks.funcional;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
@@ -7,6 +9,8 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 
 
@@ -29,4 +33,29 @@ public class TasksTest {
 		Assert.assertEquals("Sucess!", mensagem);
 		driver.quit();
 	}
+	@Test
+	public void deveSalvarTarefaComSucessoSeleniumGrid() throws MalformedURLException {
+		
+		
+		WebDriver driver = acessarAplicacao();
+		driver.findElement(By.id("addTodo")).click();
+		driver.findElement(By.id("task")).sendKeys("Teste via selenium remoto");
+		driver.findElement(By.id("dueDate")).sendKeys("10/10/2031");
+		
+		driver.findElement(By.id("saveButton")).click();
+		driver.manage().timeouts().implicitlyWait(40,TimeUnit.SECONDS);
+		String mensagem=driver.findElement(By.id("message")).getText();
+		System.out.println("Mensagem!!!!=>"+mensagem);
+		Assert.assertEquals("Sucess!", mensagem);
+		driver.quit();
+	}
+	public WebDriver acessarAplicacao() throws MalformedURLException {
+		String iplocal = "http://192.168.0.13";
+		DesiredCapabilities cap = DesiredCapabilities.chrome();
+		WebDriver driver = new RemoteWebDriver(new URL(iplocal+":4444/wd/hub"), cap);
+		driver.navigate().to(iplocal+":8001/tasks");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		return driver;
+	}
+
 }
